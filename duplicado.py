f3a234d42b39ed34d818565e27a0fc2d3aa31125
JeufoDev <117694456+JeufoDev@users.noreply.github.com>
2023-02-15 20:40:24 -0300

    Melhorar o código e sua complexidade

--- duplicado.py
+++ duplicado.py
@@ -1,24 +1,14 @@
 def duplicado(lista):
-    #contador de respostas duplicadas
-    resp = 0
-    for i in range(len(lista)):
-        #contador que sempre vai zerar com o inicio do laço
-        cont = 0
-        #variavel auxiliar que vai receber resutado por resultado da lista
-        aux = lista[i]
-        #vai percorer os valores da lista e comparar com a variavel aux
-        for e in lista:
-            #caso apareça mais de uam vez ele vai somar 1 no contador
-            if aux == e:
-                cont+=1
-            #se o contador tiver mais de 1 significa dzr que algum numero se repetiu, entao adiciona 1 na resposta
-            if cont > 1:
-                resp +=1
-    #se a resposta for diferente de zero, entao sabemos que existe numeros repetidos
-    if resp != 0:
-        print("Duplicado")
-    else:
-        print("Não duplicado")
+    for i in range(len(lista)-1):
+        for j in range(i+1,len(lista)):
+            if lista[i] == lista[j]:
+                return True
+    return False
 
 
-duplicado([1,2])
+a = duplicado([1,2,5,6])
+
+if a == True:
+    print("Duplicado")
+else:
+    print("Não duplicado")
